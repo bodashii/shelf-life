2fa6bf8f188e85da96edb32bb89b36513c6080b0
JoDamaso <darryldamaso@gmail.com>
2022-04-01 11:40:11 -0500

    added to user model and user route

--- models/User.js
+++ models/User.js
@@ -1,15 +1,48 @@
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection.js');
+// const bcrypt = require('bcrypt');
 
-class User extends Model {}
+class User extends Model {
+    // have to include our bcrypt
+}
 
 User.init(
     {
         id: {
-
-        }
+            // use the special Sequelize DataTypes object provide what type of data it is
+            type: DataTypes.INTEGER,
+            // this is the equivalent of SQL's `NOT NULL` option
+            allowNull: false,
+            // instruct that this is the Primary Key
+            primaryKey: true,
+            // turn on auto increment
+            autoIncrement: true
+        },
+        username: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true,
+            validate: {
+                isEmail: true
+            }
+        },
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+            // this means the password must be at least four characters long
+            len: [4]
+            }
+        },
     },
     {
+        hooks: {
+            // set up beforeCreate lifecycle "hook" functionality
+        },
         sequelize,
         freezeTableName: true,
         underscored: true,
